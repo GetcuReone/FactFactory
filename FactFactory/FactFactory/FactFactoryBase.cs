84adbe035a0c6ec790ae607d585092b406f5ec97
Getcu Reone <GetcuReone@hotmail.com>
2020-01-02 17:08:56 +0300

    (dev-1) create test project

---
+++ FactFactory/FactFactory/FactFactoryBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ComboPatterns.Interfaces;
+
+namespace FactFactory
+{
+    /// <summary>
+    /// Base class for fact factory
+    /// </summary>
+    public abstract class FactFactoryBase : IAbstractFactory
+    {
+        /// <summary>
+        /// Object creation method
+        /// </summary>
+        /// <typeparam name="TParameters"></typeparam>
+        /// <typeparam name="TObj"></typeparam>
+        /// <param name="factoryFunc"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public virtual TObj CreateObject<TParameters, TObj>(Func<TParameters, TObj> factoryFunc, TParameters parameters)
+        {
+            return factoryFunc != null
+                ? factoryFunc(parameters)
+                : throw new ArgumentNullException(nameof(factoryFunc));
+        }
+    }
+}
---
+++ FactFactory/FactFactoryTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactFactoryTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            System.Console.Write("Hello");
+        }
+    }
+}
