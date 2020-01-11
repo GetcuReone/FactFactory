b8bd92ca4cc7eb0bea79964935d147fe7672cf43
Getcu Reone <GetcuReone@hotmail.com>
2020-01-11 03:22:37 +0300

    (dev-1) added test

--- FactFactory/FactFactoryTests/FactFactoryT/Env/FactFactoryWithoutRules.cs
+++ FactFactory/FactFactoryTests/FactFactoryT/Env/FactFactoryWithoutRules.cs
@@ -1,11 +1,12 @@
 ﻿using FactFactory.Entities;
 using System.Collections.Generic;
+using WAction = FactFactory.Entities.WantAction;
 
 namespace FactFactoryTests.FactFactoryT.Env
 {
     internal sealed class FactFactoryWithoutRules : FactFactory.FactFactory
     {
-        public override IEnumerable<FactFactory.Entities.FactRule> GetRulesForDerive(WantAction wantAction)
+        public override IEnumerable<FactFactory.Entities.FactRule> GetRulesForDerive(WAction wantAction)
         {
             return default;
         }
---
+++ FactFactory/FactFactoryTests/WantAction/WantActionTests.cs
@@ -0,0 +1,35 @@
+﻿using FactFactory.Interfaces;
+using JwtTestAdapter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using WAction = FactFactory.Entities.WantAction;
+
+namespace FactFactoryTests.WantAction
+{
+    [TestClass]
+    public sealed class WantActionTests : TestBase
+    {
+        [Timeout(Timeouits.MilliSecond.Hundred)]
+        [TestMethod]
+        [Description("[fact][action][negative] create WantAction without action")]
+        public void CreateWantActionWithoutActionTestCase()
+        {
+            GivenEmpty()
+                .When("Create WantAction", _ => ExpectedException<ArgumentNullException>(() => new WAction(null, null)))
+                .Then("Check error", ex => Assert.IsNotNull(ex, "error is null"));
+        }
+
+        [Timeout(Timeouits.MilliSecond.Hundred)]
+        [TestMethod]
+        [Description("[fact][action][negative] run invoke")]
+        public void InvokeTestCase()
+        {
+            bool isRun = false;
+
+            Given("Create WantAction", () => new WAction(ct => isRun = true, new List<IFactInfo>()))
+                .When("Run method", wantAction => wantAction.Invoke(new FactFactory.Entities.FactContainer()))
+                .Then("Check result", _ => Assert.IsTrue(isRun, "Invoke not run"));
+        }
+    }
+}
