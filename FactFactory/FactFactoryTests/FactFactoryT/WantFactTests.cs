cc48677c92aceab010a17ef295ad16971d84454d
Getcu Reone <GetcuReone@hotmail.com>
2020-01-17 03:00:13 +0300

    (dev-1) added tests method WantFact

--- FactFactory/FactFactoryTests/FactFactoryT/WantFactTests.cs
+++ FactFactory/FactFactoryTests/FactFactoryT/WantFactTests.cs
@@ -1,6 +1,8 @@
 ﻿using FactFactory.Consts;
+using FactFactory.Entities;
 using FactFactory.Exceptions;
 using FactFactory.Facts;
+using FactFactoryTests.CommonFacts;
 using JwtTestAdapter;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -22,9 +24,47 @@ namespace FactFactoryTests.FactFactoryT
                     Assert.IsNotNull(ex.Details, "error cannot be null");
                     Assert.AreEqual(1, ex.Details.Count, "Details must contain 1 detail");
 
-                    var detail = ex.Details[0];
+                    ErrorDetail detail = ex.Details[0];
                     Assert.AreEqual(ErrorCodes.InvalidData, detail.Code, "code not match");
                     Assert.AreEqual("The CurrentFactsFindingFact is available only for the rules", detail.Reason, "reason not match");
+                });
+        }
+
+        [Timeout(Timeouits.MilliSecond.Hundred)]
+        [TestMethod]
+        [Description("[fact][factory][negative] Want not contained fact")]
+        public void WantNotContainedFactTestCase()
+        {
+            GivenCreateFactFactory()
+                .When("NotContainedFact", factory => ExpectedException<FactFactoryException>(() => factory.WantFact((NotContained<Input1Fact> _) => { })))
+                .Then("Check error", ex =>
+                {
+                    Assert.IsNotNull(ex, "error cannot be null");
+                    Assert.IsNotNull(ex.Details, "Details cannot be null");
+                    Assert.AreEqual(1, ex.Details.Count, "there must be one detail");
+
+                    ErrorDetail detail = ex.Details[0];
+                    Assert.AreEqual(ErrorCodes.InvalidData, detail.Code, "code not match");
+                    Assert.AreEqual("Cannot derive for No and NotContained facts", detail.Reason, "reason not match");
+                });
+        }
+
+        [Timeout(Timeouits.MilliSecond.Hundred)]
+        [TestMethod]
+        [Description("[fact][factory][negative] Want no fact")]
+        public void WantNoFactTestCase()
+        {
+            GivenCreateFactFactory()
+                .When("NotContainedFact", factory => ExpectedException<FactFactoryException>(() => factory.WantFact((No<Input1Fact> _) => { })))
+                .Then("Check error", ex =>
+                {
+                    Assert.IsNotNull(ex, "error cannot be null");
+                    Assert.IsNotNull(ex.Details, "Details cannot be null");
+                    Assert.AreEqual(1, ex.Details.Count, "there must be one detail");
+
+                    ErrorDetail detail = ex.Details[0];
+                    Assert.AreEqual(ErrorCodes.InvalidData, detail.Code, "code not match");
+                    Assert.AreEqual("Cannot derive for No and NotContained facts", detail.Reason, "reason not match");
                 });
         }
     }
