8071677581bcd2788b22901c92cf713baa00a4d4
Getcu Reone <GetcuReone@hotmail.com>
2020-01-18 04:10:29 +0300

    (dev-1) added test DeriveWithNoFactTestCase

--- FactFactory/FactFactoryTests/FactFactoryT/NoTests.cs
+++ FactFactory/FactFactoryTests/FactFactoryT/NoTests.cs
@@ -29,5 +29,27 @@ namespace FactFactoryTests.FactFactoryT
                     Assert.AreEqual(3, fact.Value, "fact have other value");
                 });
         }
+
+        [Timeout(Timeouits.MilliSecond.Hundred)]
+        [TestMethod]
+        [Description("[fact][factory] derive with NoFact")]
+        public void DeriveWithNoFactTestCase()
+        {
+            GivenCreateFactFactory()
+                .AndRulesNotNul()
+                .And("Add rules", factory => 
+                {
+                    factory.Rules.Add((Input12Fact fact) => new Input11Fact(fact.Value + 11));
+                    factory.Rules.Add((Input14Fact fact, No<Input9Fact> no) => new Input12Fact(fact.Value + 12));
+                    factory.Rules.Add((Input8Fact fact) => new Input9Fact(fact.Value + 12));
+                })
+                .And("Add container", factory => factory.Container.Add(new Input14Fact(14)))
+                .When("Derive", factory => factory.DeriveFact<Input11Fact>())
+                .Then("Check fact", fact =>
+                {
+                    Assert.IsNotNull(fact, "fact cannot be null");
+                    Assert.AreEqual(37, fact.Value, "fact have other value");
+                });
+        }
     }
 }
