b4508550634cd33571bd6c1a075fcb4fe5cf867a
GetcuReone <getcureone@hotmail.com>
2020-02-22 21:39:56 +0300

    added test CreateRuleWithoutVersionTestCase

--- FactFactory/VersionedFactFactory/FactFactory.VersionedTests/VersionedFactRule/VersionedFactRuleTests.cs
+++ FactFactory/VersionedFactFactory/FactFactory.VersionedTests/VersionedFactRule/VersionedFactRuleTests.cs
@@ -25,8 +25,21 @@ namespace FactFactory.VersionedTests.VersionedFactRule
                 .When("Create rule with version", _ => VersionedFactRuleHelper.CreateRule(GetFactType<V1>(), GetFactType<Fact1>()))
                 .Then("Check result", rule =>
                 {
-                    Assert.IsNotNull(rule.TypeFactVersion, "No rule version information");
+                    Assert.IsNotNull(rule.TypeFactVersion, "The rule does not contain version information");
                     Assert.IsTrue(GetFactType<V1>().Compare(rule.TypeFactVersion), $"{nameof(Rule.TypeFactVersion)} does not store version information");
+                });
+        }
+
+        [Timeout(Timeouts.MilliSecond.Hundred)]
+        [TestMethod]
+        [Description("[fact][versioned][rule] create rule without version")]
+        public void CreateRuleWithoutVersionTestCase()
+        {
+            GivenEmpty()
+                .When("Create rule with version", _ => VersionedFactRuleHelper.CreateRule(GetFactType<Fact1>()))
+                .Then("Check result", rule =>
+                {
+                    Assert.IsNull(rule.TypeFactVersion, "The rule contain version information");
                 });
         }
     }
