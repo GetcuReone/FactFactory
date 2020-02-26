a53ca953b50ce8f6eb22b729923a3199647389eb
GetcuReone <getcureone@hotmail.com>
2020-02-26 20:34:17 +0300

    #21 change FactRule

--- FactFactory/FactFactory/Entities/FactRule.cs
+++ FactFactory/FactFactory/Entities/FactRule.cs
@@ -1,118 +1,25 @@
-﻿using GetcuReone.FactFactory.Helpers;
-using GetcuReone.FactFactory.Interfaces;
+﻿using GetcuReone.FactFactory.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
 
 namespace GetcuReone.FactFactory.Entities
 {
     /// <summary>
     /// Rule of fact calculation
     /// </summary>
-    public class FactRule : IFactRule
+    public class FactRule : FactRuleBase<IFact, IFactContainer<IFact>>
     {
-        private readonly Func<IFactContainer, IFact> _func;
-
         /// <summary>
-        /// Information on input factacles rules
+        /// Constructor.
         /// </summary>
-        public IReadOnlyCollection<IFactType> InputFactTypes { get; }
-
-        /// <summary>
-        /// Information on output fact
-        /// </summary>
-        public IFactType OutputFactType { get; }
-
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="func">func for calculate</param>
-        /// <param name="inputFactTypes">information on input factacles rules</param>
-        /// <param name="outputFactType">information on output fact</param>
-        public FactRule(Func<IFactContainer, IFact> func, List<IFactType> inputFactTypes, IFactType outputFactType)
+        /// <param name="func">Func for calculate.</param>
+        /// <param name="inputFactTypes">Information on input factacles rules.</param>
+        /// <param name="outputFactType">Information on output fact.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="func"/> or <paramref name="outputFactType"/> is null.</exception>
+        /// <exception cref="ArgumentException">The fact is requested at the input, which the rule calculates.</exception>
+        public FactRule(Func<IFactContainer<IFact>, IFact> func, List<IFactType> inputFactTypes, IFactType outputFactType) 
+            : base(func, inputFactTypes, outputFactType)
         {
-            _func = func ?? throw new ArgumentNullException(nameof(func));
-            InputFactTypes = inputFactTypes != null 
-                ? new ReadOnlyCollection<IFactType>(inputFactTypes)
-                : new ReadOnlyCollection<IFactType>(new List<IFactType>());
-            OutputFactType = outputFactType ?? throw new ArgumentNullException(nameof(outputFactType));
-
-            if (InputFactTypes.Any(factType => factType.Compare(OutputFactType)))
-                throw new ArgumentException("Cannot request a fact calculated according to the rule");
-        }
-
-        /// <summary>
-        /// Fact type set comparison
-        /// </summary>
-        /// <param name="first"></param>
-        /// <param name="second"></param>
-        /// <returns></returns>
-        protected virtual bool CompareFactTypes(IEnumerable<IFactType> first, IEnumerable<IFactType> second)
-        {
-            if (first.IsNullOrEmpty() && second.IsNullOrEmpty())
-                return true;
-            else if (first.IsNullOrEmpty() || second.IsNullOrEmpty())
-                return false;
-            else if (first.Count() != second.Count())
-                return false;
-            else
-            {
-                foreach (var fact in second)
-                {
-                    if (first.All(f => !f.Compare(fact)))
-                        return false;
-                }
-
-                return true;
-            }
-        }
-
-        /// <summary>
-        /// Rule of fact calculate
-        /// </summary>
-        /// <param name="container"></param>
-        /// <returns></returns>
-        public virtual IFact Calculate<TFactContainer>(TFactContainer container) 
-            where TFactContainer : IFactContainer
-        {
-            IFact fact = _func(container);
-
-            if (fact == null)
-                throw new InvalidOperationException("Rule cannot return null");
-
-            return fact;
-        }
-
-        /// <summary>
-        /// is it possible to calculate the fact
-        /// </summary>
-        /// <param name="container"></param>
-        /// <returns></returns>
-        public virtual bool CanCalculate<TFactContainer>(TFactContainer container) 
-            where TFactContainer : IFactContainer
-        {
-            return InputFactTypes.All(factInfo => factInfo.ContainsContainer(container));
-        }
-
-        /// <summary>
-        /// Compare rules
-        /// </summary>
-        /// <typeparam name="TFactRule"></typeparam>
-        /// <param name="factRule"></param>
-        /// <returns></returns>
-        public bool Compare<TFactRule>(TFactRule factRule) where TFactRule : IFactRule
-        {
-            if (!OutputFactType.Compare(factRule.OutputFactType))
-                return false;
-
-            return CompareFactTypes(factRule.InputFactTypes, InputFactTypes);
-        }
-
-        /// <inheritdoc />
-        public override string ToString()
-        {
-            return $"({string.Join(", ", InputFactTypes.Select(f => f.FactName).ToList())}) => ({OutputFactType.FactName})";
         }
     }
 }
