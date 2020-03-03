58e579fe7d1b3eb041a1da052ea0091167db391b
GetcuReone <getcureone@hotmail.com>
2020-03-04 00:37:33 +0300

    #31 added UintVersionBase and tests

--- FactFactory/VersionedFactFactory/FactFactory.VersionedTests/Version/UnitVersionTests.cs
+++ FactFactory/VersionedFactFactory/FactFactory.VersionedTests/Version/UnitVersionTests.cs
@@ -6,19 +6,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FactFactory.VersionedTests.Version
 {
     [TestClass]
-    public sealed class LongVersionTests : TestBase
+    public sealed class UintVersionTests : TestBase
     {
         [TestMethod]
         [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact)]
         [Description("The first version is less than the second")]
         [Timeout(Timeouts.MilliSecond.Hundred)]
-        public void LongVersion_FirstVersionLessThanSecondTestCase()
+        public void UintVersion_FirstVersionLessThanSecondTestCase()
         {
-            LongVersion v1 = null;
-            LongVersion v2 = null;
+            UintVersion v1 = null;
+            UintVersion v2 = null;
 
-            Given("Create first version", () => v1 = new LongVersion(1))
-                .And("Create second version", _ => v2 = new LongVersion(2))
+            Given("Create first version", () => v1 = new UintVersion(1))
+                .And("Create second version", _ => v2 = new UintVersion(2))
                 .When("Compare version", _ => v1.IsLessThan(v2))
                 .Then("Check result", result => Assert.IsTrue(result, "The first version is not less than the second"));
         }
@@ -27,75 +27,75 @@ namespace FactFactory.VersionedTests.Version
         [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact)]
         [Description("The second version is more than the first")]
         [Timeout(Timeouts.MilliSecond.Hundred)]
-        public void LongVersion_SecondVersionMoreThanFirstTestCase()
+        public void UintVersion_SecondVersionMoreThanFirstTestCase()
         {
-            LongVersion v1 = null;
-            LongVersion v2 = null;
+            UintVersion v1 = null;
+            UintVersion v2 = null;
 
-            Given("Create first version", () => v1 = new LongVersion(1))
-                .And("Create second version", _ => v2 = new LongVersion(2))
+            Given("Create first version", () => v1 = new UintVersion(1))
+                .And("Create second version", _ => v2 = new UintVersion(2))
                 .When("Compare version", _ => v2.IsMoreThan(v1))
                 .Then("Check result", result => Assert.IsTrue(result, "The first version is not more than the second"));
         }
 
         [TestMethod]
         [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact)]
-        [Description("The Long version is not less than the DateTime")]
+        [Description("The Uint version is not less than the DateTime")]
         [Timeout(Timeouts.MilliSecond.Hundred)]
-        public void LongVersionVersionNotLessThanDateTimeTestCase()
+        public void UintVersionVersionNotLessThanDateTimeTestCase()
         {
-            LongVersion v1 = null;
+            UintVersion v1 = null;
             Version2020 v2 = null;
 
-            Given("Create first version", () => v1 = new LongVersion(1))
+            Given("Create first version", () => v1 = new UintVersion(1))
                 .And("Create second version", _ => v2 = new Version2020())
                 .When("Compare version", _ => v1.IsLessThan(v2))
-                .Then("Check result", result => Assert.IsFalse(result, "The Long version is less than the DateTime"));
+                .Then("Check result", result => Assert.IsFalse(result, "The Uint version is less than the DateTime"));
         }
 
         [TestMethod]
         [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact)]
-        [Description("The Long version is not more than the DateTime")]
+        [Description("The Uint version is not more than the DateTime")]
         [Timeout(Timeouts.MilliSecond.Hundred)]
-        public void LongVersionNotMoreThanDateTimeTestCase()
+        public void UintVersionNotMoreThanDateTimeTestCase()
         {
-            LongVersion v1 = null;
+            UintVersion v1 = null;
             Version2020 v2 = null;
 
-            Given("Create first version", () => v1 = new LongVersion(1))
+            Given("Create first version", () => v1 = new UintVersion(1))
                 .And("Create second version", _ => v2 = new Version2020())
                 .When("Compare version", _ => v1.IsMoreThan(v2))
-                .Then("Check result", result => Assert.IsFalse(result, "The Long version is more than the DateTime"));
+                .Then("Check result", result => Assert.IsFalse(result, "The Uint version is more than the DateTime"));
         }
 
         [TestMethod]
         [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact)]
         [Description("The second version is equal the first")]
         [Timeout(Timeouts.MilliSecond.Hundred)]
-        public void LongVersion_SecondVersionEqualFirstTestCase()
+        public void UintVersion_SecondVersionEqualFirstTestCase()
         {
-            LongVersion v1 = null;
-            LongVersion v2 = null;
+            UintVersion v1 = null;
+            UintVersion v2 = null;
 
-            Given("Create first version", () => v1 = new LongVersion(1))
-                .And("Create second version", _ => v2 = new LongVersion(1))
+            Given("Create first version", () => v1 = new UintVersion(1))
+                .And("Create second version", _ => v2 = new UintVersion(1))
                 .When("Compare version", _ => v2.EqualVersion(v1))
                 .Then("Check result", result => Assert.IsTrue(result, "The first version is not equal the second"));
         }
 
         [TestMethod]
         [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact)]
-        [Description("The Long version is not equal the DateTime")]
+        [Description("The Uint version is not equal the DateTime")]
         [Timeout(Timeouts.MilliSecond.Hundred)]
-        public void LongVersionNotEqualDateTimeTestCase()
+        public void UintVersionNotEqualDateTimeTestCase()
         {
-            LongVersion v1 = null;
+            UintVersion v1 = null;
             Version2020 v2 = null;
 
-            Given("Create first version", () => v1 = new LongVersion(1))
+            Given("Create first version", () => v1 = new UintVersion(1))
                 .And("Create second version", _ => v2 = new Version2020())
                 .When("Compare version", _ => v1.Equals(v2))
-                .Then("Check result", result => Assert.IsFalse(result, "The Long version is equal the DateTime"));
+                .Then("Check result", result => Assert.IsFalse(result, "The Uint version is equal the DateTime"));
         }
     }
 }
