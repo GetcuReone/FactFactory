7e8a5b0dad21804e989b2329bbc79df94b1e838f
GetcuReone <getcureone@hotmail.com>
2020-03-03 23:09:34 +0300

    #31 added tests IntVersionTests

---
+++ FactFactory/VersionedFactFactory/FactFactory.VersionedTests/Version/IntVersionTests.cs
@@ -0,0 +1,101 @@
+﻿using FactFactory.TestsCommon;
+using FactFactory.VersionedTests.Version.Env;
+using GivenWhenThen.TestAdapter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactFactory.VersionedTests.Version
+{
+    [TestClass]
+    public sealed class IntVersionTests : TestBase
+    {
+        [TestMethod]
+        [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact)]
+        [Description("The first version is less than the second")]
+        [Timeout(Timeouts.MilliSecond.Hundred)]
+        public void IntVersion_FirstVersionLessThanSecondTestCase()
+        {
+            IntVersion v1 = null;
+            IntVersion v2 = null;
+
+            Given("Create first version", () => v1 = new IntVersion(1))
+                .And("Create second version", _ => v2 = new IntVersion(2))
+                .When("Compare version", _ => v1.IsLessThan(v2))
+                .Then("Check result", result => Assert.IsTrue(result, "The first version is not less than the second"));
+        }
+
+        [TestMethod]
+        [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact)]
+        [Description("The second version is more than the first")]
+        [Timeout(Timeouts.MilliSecond.Hundred)]
+        public void IntVersion_SecondVersionMoreThanFirstTestCase()
+        {
+            IntVersion v1 = null;
+            IntVersion v2 = null;
+
+            Given("Create first version", () => v1 = new IntVersion(1))
+                .And("Create second version", _ => v2 = new IntVersion(2))
+                .When("Compare version", _ => v2.IsMoreThan(v1))
+                .Then("Check result", result => Assert.IsTrue(result, "The first version is not more than the second"));
+        }
+
+        [TestMethod]
+        [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact)]
+        [Description("The Int version is not less than the DateTime")]
+        [Timeout(Timeouts.MilliSecond.Hundred)]
+        public void IntVersionVersionNotLessThanDateTimeTestCase()
+        {
+            IntVersion v1 = null;
+            Version2020 v2 = null;
+
+            Given("Create first version", () => v1 = new IntVersion(1))
+                .And("Create second version", _ => v2 = new Version2020())
+                .When("Compare version", _ => v1.IsLessThan(v2))
+                .Then("Check result", result => Assert.IsFalse(result, "The Int version is less than the DateTime"));
+        }
+
+        [TestMethod]
+        [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact)]
+        [Description("The Int version is not more than the DateTime")]
+        [Timeout(Timeouts.MilliSecond.Hundred)]
+        public void IntVersionNotMoreThanDateTimeTestCase()
+        {
+            IntVersion v1 = null;
+            Version2020 v2 = null;
+
+            Given("Create first version", () => v1 = new IntVersion(1))
+                .And("Create second version", _ => v2 = new Version2020())
+                .When("Compare version", _ => v1.IsMoreThan(v2))
+                .Then("Check result", result => Assert.IsFalse(result, "The Int version is more than the DateTime"));
+        }
+
+        [TestMethod]
+        [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact)]
+        [Description("The second version is equal the first")]
+        [Timeout(Timeouts.MilliSecond.Hundred)]
+        public void IntVersion_SecondVersionEqualFirstTestCase()
+        {
+            IntVersion v1 = null;
+            IntVersion v2 = null;
+
+            Given("Create first version", () => v1 = new IntVersion(1))
+                .And("Create second version", _ => v2 = new IntVersion(1))
+                .When("Compare version", _ => v2.EqualVersion(v1))
+                .Then("Check result", result => Assert.IsTrue(result, "The first version is not equal the second"));
+        }
+
+        [TestMethod]
+        [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact)]
+        [Description("The Int version is not equal the DateTime")]
+        [Timeout(Timeouts.MilliSecond.Hundred)]
+        public void IntVersionNotEqualDateTimeTestCase()
+        {
+            IntVersion v1 = null;
+            Version2020 v2 = null;
+
+            Given("Create first version", () => v1 = new IntVersion(1))
+                .And("Create second version", _ => v2 = new Version2020())
+                .When("Compare version", _ => v1.Equals(v2))
+                .Then("Check result", result => Assert.IsFalse(result, "The Int version is equal the DateTime"));
+        }
+    }
+}
