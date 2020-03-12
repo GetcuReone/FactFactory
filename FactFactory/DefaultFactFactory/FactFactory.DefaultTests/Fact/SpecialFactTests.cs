3223da1096d0f3096ea38065bddc71b64a378f8d
GetcuReone <getcureone@hotmail.com>
2020-03-12 17:49:30 +0300

    #55 added tests

---
+++ FactFactory/DefaultFactFactory/FactFactory.DefaultTests/Fact/SpecialFactTests.cs
@@ -0,0 +1,55 @@
+﻿using FactFactory.TestsCommon;
+using FactFactoryTests.CommonFacts;
+using GetcuReone.FactFactory.Default.SpecialFacts;
+using GetcuReone.FactFactory.Entities;
+using GivenWhenThen.TestAdapter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactFactory.DefaultTests.Fact
+{
+    [TestClass]
+    public sealed class SpecialFactTests : TestBase
+    {
+        [TestMethod]
+        [TestCategory(TC.Objects.Fact), TestCategory(TC.Objects.NotContained)]
+        [Description("Get FactType for NotContained fact")]
+        [Timeout(Timeouts.MilliSecond.Hundred)]
+        public void GetFactTypeForNotContainedFactTestCase()
+        {
+            GivenEmpty()
+                .When("Create NotContainet", () => new NotContained<ResultFact>())
+                .Then("Check fact type", fact =>
+                {
+                    Assert.IsTrue(fact.GetFactType() is FactType<NotContained<ResultFact>>, "Expected another FactType.");
+                });
+        }
+
+        [TestMethod]
+        [TestCategory(TC.Objects.Fact), TestCategory(TC.Objects.Contained)]
+        [Description("Get FactType for Contained fact")]
+        [Timeout(Timeouts.MilliSecond.Hundred)]
+        public void GetFactTypeForContainedFactTestCase()
+        {
+            GivenEmpty()
+                .When("Create NotContainet", () => new Contained<ResultFact>())
+                .Then("Check fact type", fact =>
+                {
+                    Assert.IsTrue(fact.GetFactType() is FactType<Contained<ResultFact>>, "Expected another FactType.");
+                });
+        }
+
+        [TestMethod]
+        [TestCategory(TC.Objects.Fact), TestCategory(TC.Objects.NoDerived)]
+        [Description("Get FactType for NoDerived fact")]
+        [Timeout(Timeouts.MilliSecond.Hundred)]
+        public void GetFactTypeForNoDerivedFactTestCase()
+        {
+            GivenEmpty()
+                .When("Create NotContainet", () => new NoDerived<ResultFact>())
+                .Then("Check fact type", fact =>
+                {
+                    Assert.IsTrue(fact.GetFactType() is FactType<NoDerived<ResultFact>>, "Expected another FactType.");
+                });
+        }
+    }
+}
---
+++ FactFactory/VersionedFactFactory/FactFactory.VersionedTests/Fact/SpecialFactTests.cs
@@ -0,0 +1,55 @@
+﻿using FactFactory.TestsCommon;
+using FactFactory.VersionedTests.CommonFacts;
+using GetcuReone.FactFactory.Default.SpecialFacts;
+using GetcuReone.FactFactory.Entities;
+using GivenWhenThen.TestAdapter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactFactory.DefaultTests.Fact
+{
+    [TestClass]
+    public sealed class SpecialFactTests : TestBase
+    {
+        [TestMethod]
+        [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact), TestCategory(TC.Objects.NotContained)]
+        [Description("Get FactType for NotContained fact")]
+        [Timeout(Timeouts.MilliSecond.Hundred)]
+        public void Versioned_GetFactTypeForNotContainedFactTestCase()
+        {
+            GivenEmpty()
+                .When("Create NotContainet", () => new NotContained<FactResult>())
+                .Then("Check fact type", fact =>
+                {
+                    Assert.IsTrue(fact.GetFactType() is FactType<NotContained<FactResult>>, "Expected another FactType.");
+                });
+        }
+
+        [TestMethod]
+        [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact), TestCategory(TC.Objects.Contained)]
+        [Description("Get FactType for Contained fact")]
+        [Timeout(Timeouts.MilliSecond.Hundred)]
+        public void Versioned_GetFactTypeForContainedFactTestCase()
+        {
+            GivenEmpty()
+                .When("Create NotContainet", () => new Contained<FactResult>())
+                .Then("Check fact type", fact =>
+                {
+                    Assert.IsTrue(fact.GetFactType() is FactType<Contained<FactResult>>, "Expected another FactType.");
+                });
+        }
+
+        [TestMethod]
+        [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact), TestCategory(TC.Objects.NoDerived)]
+        [Description("Get FactType for NoDerived fact")]
+        [Timeout(Timeouts.MilliSecond.Hundred)]
+        public void Versioned_GetFactTypeForNoDerivedFactTestCase()
+        {
+            GivenEmpty()
+                .When("Create NotContainet", () => new NoDerived<FactResult>())
+                .Then("Check fact type", fact =>
+                {
+                    Assert.IsTrue(fact.GetFactType() is FactType<NoDerived<FactResult>>, "Expected another FactType.");
+                });
+        }
+    }
+}
