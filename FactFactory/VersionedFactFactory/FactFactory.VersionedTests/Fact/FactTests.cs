2f5c8e9cc81cb70872e1bc86d39eef53ed3162f8
GetcuReone <getcureone@hotmail.com>
2020-03-12 17:37:22 +0300

    #55 added Versioned_SetValueTestCase

---
+++ FactFactory/VersionedFactFactory/FactFactory.VersionedTests/Fact/FactTests.cs
@@ -0,0 +1,27 @@
+﻿using FactFactory.TestsCommon;
+using FactFactory.VersionedTests.CommonFacts;
+using GivenWhenThen.TestAdapter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactFactory.VersionedTests.Fact
+{
+    [TestClass]
+    public sealed class FactTests : TestBase
+    {
+        [TestMethod]
+        [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Fact)]
+        [Description("Set fact value")]
+        [Timeout(Timeouts.MilliSecond.Hundred)]
+        public void Versioned_SetValueTestCase()
+        {
+            long factValue = 5;
+
+            GivenEmpty()
+                .When("Create fact", _ => new FactResult(factValue))
+                .Then("Check fact value", fact =>
+                {
+                    Assert.AreEqual(factValue, fact.Value, "Expected another fact value.");
+                });
+        }
+    }
+}
