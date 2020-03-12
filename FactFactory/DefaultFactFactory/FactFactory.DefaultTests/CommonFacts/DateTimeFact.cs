9adc03ed9ac19e2bb568bdee0eb01a911806903f
GetcuReone <getcureone@hotmail.com>
2020-03-12 15:33:39 +0300

    #54 move coomon facts for tests

---
+++ FactFactory/DefaultFactFactory/FactFactory.DefaultTests/CommonFacts/DateTimeFact.cs
@@ -0,0 +1,12 @@
+﻿using GetcuReone.FactFactory.Default;
+using System;
+
+namespace FactFactoryTests.CommonFacts
+{
+    internal sealed class DateTimeFact : FactBase<DateTime>
+    {
+        public DateTimeFact(DateTime fact) : base(fact)
+        {
+        }
+    }
+}
---
+++ FactFactory/DefaultFactFactory/FactFactory.DefaultTests/CommonFacts/Input1Fact.cs
@@ -0,0 +1,102 @@
+﻿using GetcuReone.FactFactory.Default;
+
+namespace FactFactoryTests.CommonFacts
+{
+    internal sealed class Input1Fact : FactBase<int>
+    {
+        public Input1Fact(int fact) : base(fact)
+        {
+        }
+    }
+    internal sealed class Input2Fact : FactBase<int>
+    {
+        public Input2Fact(int fact) : base(fact)
+        {
+        }
+    }
+    internal sealed class Input3Fact : FactBase<int>
+    {
+        public Input3Fact(int fact) : base(fact)
+        {
+        }
+    }
+    internal sealed class Input4Fact : FactBase<int>
+    {
+        public Input4Fact(int fact) : base(fact)
+        {
+        }
+    }
+    internal sealed class Input5Fact : FactBase<int>
+    {
+        public Input5Fact(int fact) : base(fact)
+        {
+        }
+    }
+    internal sealed class Input6Fact : FactBase<int>
+    {
+        public Input6Fact(int fact) : base(fact)
+        {
+        }
+    }
+    internal sealed class Input7Fact : FactBase<int>
+    {
+        public Input7Fact(int fact) : base(fact)
+        {
+        }
+    }
+    internal sealed class Input8Fact : FactBase<int>
+    {
+        public Input8Fact(int fact) : base(fact)
+        {
+        }
+    }
+    internal sealed class Input9Fact : FactBase<int>
+    {
+        public Input9Fact(int fact) : base(fact)
+        {
+        }
+    }
+    internal sealed class Input10Fact : FactBase<int>
+    {
+        public Input10Fact(int fact) : base(fact)
+        {
+        }
+    }
+    internal sealed class Input11Fact : FactBase<int>
+    {
+        public Input11Fact(int fact) : base(fact)
+        {
+        }
+    }
+    internal sealed class Input12Fact : FactBase<int>
+    {
+        public Input12Fact(int fact) : base(fact)
+        {
+        }
+    }
+    internal sealed class Input13Fact : FactBase<int>
+    {
+        public Input13Fact(int fact) : base(fact)
+        {
+        }
+    }
+    internal sealed class Input14Fact : FactBase<int>
+    {
+        public Input14Fact(int fact) : base(fact)
+        {
+        }
+    }
+    internal sealed class Input15Fact : FactBase<int>
+    {
+        public Input15Fact(int fact) : base(fact)
+        {
+        }
+    }
+
+    internal sealed class Input16Fact : FactBase<int>
+    {
+        public Input16Fact(int fact) : base(fact)
+        {
+        }
+    }
+}
