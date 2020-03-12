8b55ae301e019e1478f46dec431e3d956ff9927b
GetcuReone <getcureone@hotmail.com>
2020-03-12 15:22:39 +0300

    #54 added base fact and special facts

---
+++ FactFactory/DefaultFactFactory/FactFactory.Default/NoDerived.cs
@@ -0,0 +1,46 @@
+﻿using GetcuReone.FactFactory.Entities;
+using GetcuReone.FactFactory.Interfaces;
+
+namespace GetcuReone.FactFactory.Default
+{
+    /// <summary>
+    /// Contains information about a type of fact that cannot be calculated
+    /// </summary>
+    public sealed class NoDerived<TFact> : FactBase<IFactType>, INoDerivedFact
+        where TFact : IFact
+    {
+        /// <summary>
+        /// Value fact.
+        /// </summary>
+        public override IFactType Value { get; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public NoDerived() : base(null)
+        {
+            Value = GetFactType();
+        }
+
+        /// <summary>
+        /// Get fact type.
+        /// </summary>
+        /// <returns>fact type</returns>
+        public override IFactType GetFactType()
+        {
+            return new FactType<NoDerived<TFact>>();
+        }
+
+        /// <summary>
+        /// Is the fact contained in the container.
+        /// </summary>
+        /// <typeparam name="TFact1"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        public bool IsFactContained<TFact1>(IFactContainer<TFact1> container)
+            where TFact1 : IFact
+        {
+            return Value.TryGetFact(container, out TFact1 _);
+        }
+    }
+}
---
+++ FactFactory/DefaultFactFactory/FactFactory.Default/NotContained.cs
@@ -0,0 +1,47 @@
+﻿using GetcuReone.FactFactory.Entities;
+using GetcuReone.FactFactory.Helpers;
+using GetcuReone.FactFactory.Interfaces;
+
+namespace GetcuReone.FactFactory.Default
+{
+    /// <summary>
+    /// Information about a fact that is not contained in the container at the time of the function call <see cref="FactFactoryBase{TFact, TFactContainer, TFactRule, TFactRuleCollection, TWantAction}.Derive"/>
+    /// </summary>
+    public sealed class NotContained<TFact> : FactBase<IFactType>, INotContainedFact
+        where TFact : IFact
+    {
+        /// <summary>
+        /// Value fact.
+        /// </summary>
+        public override IFactType Value { get; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public NotContained() : base(null)
+        {
+            Value = GetFactType();
+        }
+
+        /// <summary>
+        /// Get fact type.
+        /// </summary>
+        /// <returns>fact type</returns>
+        public override IFactType GetFactType()
+        {
+            return new FactType<NotContained<TFact>>();
+        }
+
+        /// <summary>
+        /// Is the fact contained in the container.
+        /// </summary>
+        /// <typeparam name="TFact1"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        public bool IsFactContained<TFact1>(IFactContainer<TFact1> container)
+            where TFact1 : IFact
+        {
+            return Value.TryGetFact(container, out TFact1 _);
+        }
+    }
+}
