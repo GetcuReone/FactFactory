46310c000509f0db86edfab3deb0fb4b40e7425c
GetcuReone <getcureone@hotmail.com>
2020-03-19 21:07:47 +0300

    #64 fix

--- FactFactory/VersionedFactFactory/FactFactory.Versioned/Interfaces/IVersionedFact.cs
+++ FactFactory/VersionedFactFactory/FactFactory.Versioned/Interfaces/IVersionedFact.cs
@@ -5,7 +5,7 @@ namespace GetcuReone.FactFactory.Versioned.Interfaces
     /// <summary>
     /// Versioned <see cref="IFact"/>.
     /// </summary>
-    public interface IVersionFact : IFact
+    public interface IVersionedFact : IFact
     {
         /// <summary>
         /// Version of the rule that calculated the fact.
