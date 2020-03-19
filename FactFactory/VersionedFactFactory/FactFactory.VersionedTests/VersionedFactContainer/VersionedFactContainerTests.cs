4a1953a295ae31802e81ba6ab9df525ca61b4886
GetcuReone <getcureone@hotmail.com>
2020-03-19 18:13:08 +0300

    #64 move tests for versione fact container

--- FactFactory/VersionedFactFactory/FactFactory.VersionedTests/VersionedFactContainer/VersionedFactContainerTests.cs
+++ FactFactory/VersionedFactFactory/FactFactory.VersionedTests/VersionedFactContainer/VersionedFactContainerTests.cs
@@ -22,67 +22,6 @@ namespace FactFactory.VersionedTests.VersionedFactContainer
 
         [TestMethod]
         [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Container)]
-        [Description("Add versioned fact to container.")]
-        [Timeout(Timeouts.MilliSecond.Hundred)]
-        public void AddVersionedFactContainerTestCase()
-        {
-            GivenCreateContainer()
-                .When("added versioned fact", container => container.Add(new Version1()))
-                .Then("Check result", container =>
-                {
-                    Assert.IsTrue(container.Contains<Version1>(), "Version fact not contained in container");
-                });
-        }
-
-        [TestMethod]
-        [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Container)]
-        [Description("Add two versioned facts to the container.")]
-        [Timeout(Timeouts.MilliSecond.Hundred)]
-        public void AddTwoVersionedFactsContainerTestCase()
-        {
-            GivenCreateContainer()
-                .When("added versioned fact", container => 
-                {
-                    container.Add(new Version1());
-                    container.Add(new Version2());
-                })
-                .Then("Check result", container =>
-                {
-                    Assert.IsTrue(container.Contains<Version1>(), "Version fact not contained in container");
-                    Assert.IsTrue(container.Contains<Version2>(), "Version fact not contained in container");
-                });
-        }
-
-        [TestMethod]
-        [TestCategory(TC.Negative), TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Container)]
-        [Description("Add two identical versioned facts to the container.")]
-        [Timeout(Timeouts.MilliSecond.Hundred)]
-        public void AddTwoIdenticalVersionedFactsContainerTestCase()
-        {
-            string expectedReason = $"The container already contains fact type {typeof(Version1).FullName} without version.";
-
-            GivenCreateContainer()
-                .And("first addition of versioned fact", container => container.Add(new Version1()))
-                .When("second addition of versioned fact", container => ExpectedFactFactoryException(() => container.Add(new Version1())))
-                .ThenAssertErrorDetail(ErrorCode.InvalidData, expectedReason);
-        }
-
-        [TestMethod]
-        [TestCategory(TC.Negative), TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Container)]
-        [Description("Add two facts with identical versions to the container.")]
-        [Timeout(Timeouts.MilliSecond.Hundred)]
-        public void AddTwoFactsWithIdenticalVersionsContainerTestCase()
-        {
-            string expectedReason = $"The container already contains fact type {typeof(FactResult).FullName} with version equal to version {typeof(Version1).FullName}.";
-
-            GivenCreateContainer()
-                .And("first addition of versioned fact", container => container.Add(new FactResult(0, new Version1())))
-                .When("second addition of versioned fact", container => ExpectedFactFactoryException(() => container.Add(new FactResult(0, new Version1()))))
-                .ThenAssertErrorDetail(ErrorCode.InvalidData, expectedReason);
-        }
-
-        [TestMethod]
-        [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Container)]
         [Description("Get copied container.")]
         [Timeout(Timeouts.MilliSecond.Hundred)]
         public void VersionedFactContainer_GetCopiedContainerTestCase()
