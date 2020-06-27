efe33ff2a44a19a262b4dc6e46fa41cef2502b6e
Juel <GetcuReone@hotmail.com>
2020-06-27 10:02:34 +0300

    adapt infra tests

--- FactFactory/Infrastructure/InfrastructureTests/InfrastructureTests.cs
+++ FactFactory/Infrastructure/InfrastructureTests/InfrastructureTests.cs
@@ -1,13 +1,7 @@
-using FactFactory.TestsCommon;
-using GetcuReone.GwtTestFramework;
-using GetcuReone.GwtTestFramework.Helpers;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
-using System.Linq;
-using System.Reflection;
+using GetcuReone.GetcuTestAdapter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace InfrastructureTests
 {
