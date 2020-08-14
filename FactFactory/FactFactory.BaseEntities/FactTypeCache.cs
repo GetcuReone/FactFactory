15b0bb206cb548230d57442c28e689872c48ae90
Itua Juel <getcureone@hotmail.com>
2020-08-14 18:39:48 +0300

    Added FactTypeCache

---
+++ FactFactory/FactFactory.BaseEntities/FactTypeCache.cs
@@ -0,0 +1,24 @@
+﻿using GetcuReone.FactFactory.Interfaces;
+using System.Collections.Generic;
+
+namespace GetcuReone.FactFactory.BaseEntities
+{
+    /// <summary>
+    /// Fact type cache.
+    /// </summary>
+    public class FactTypeCache : IFactTypeCache
+    {
+        private readonly Dictionary<IFact, IFactType> _cache = new Dictionary<IFact, IFactType>();
+
+        /// <inheritdoc/>
+        public IFactType GetFactType<TFact>(TFact fact) where TFact : IFact
+        {
+            if (!_cache.ContainsKey(fact))
+                return _cache[fact];
+
+            IFactType factType = fact.GetFactType();
+            _cache.Add(fact, factType);
+            return factType;
+        }
+    }
+}
