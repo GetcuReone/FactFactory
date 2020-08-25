b3b39f2ff20a9908ae57a7655e7903531f3a2682
Itua Juel <getcureone@hotmail.com>
2020-08-25 17:05:28 +0300

    Remove tests

--- FactFactory/VersionedFactFactory/FactFactory.VersionedTests/VersionedFactRule/VersionedFactRuleTests.cs
+++ FactFactory/VersionedFactFactory/FactFactory.VersionedTests/VersionedFactRule/VersionedFactRuleTests.cs
@@ -50,37 +50,6 @@ namespace FactFactory.VersionedTests.VersionedFactRule
 
         [TestMethod]
         [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Rule), TestCategory(GetcuReoneTC.Unit)]
-        [Description("Calculate fact.")]
-        [Timeout(Timeouts.Millisecond.FiveHundred)]
-        public void VersionedFactRule_CalculateTestCase()
-        {
-            Container container = null;
-
-            Given("Create container", () => container = new Container())
-                .And("Added fact version", () => container.Add(new Version2()))
-                .And("Create rule", () => new Rule(facts => new FactResult(1), new List<IFactType> { GetFactType<Version2>() }, GetFactType<FactResult>()))
-                .When("Run calculate", rule => rule.Calculate(container))
-                .ThenIsNotNull()
-                .And("Get version.", fact =>
-                {
-                    if (fact is VersionedFactBase versionedFact)
-                        return versionedFact.GetVersionOrNull();
-
-                    Assert.Fail("Invalid type.");
-                    return null;
-                })
-                .AndIsNotNull()
-                .And("Check result.", version =>
-                {
-                    if (version is Version2 version2)
-                        Assert.AreEqual(2, version2, "expected another version.");
-                    else
-                        Assert.Fail("Version is not Version2.");
-                });
-        }
-
-        [TestMethod]
-        [TestCategory(TC.Projects.Versioned), TestCategory(TC.Objects.Rule), TestCategory(GetcuReoneTC.Unit)]
         [Description("Calculate fact without version.")]
         [Timeout(Timeouts.Millisecond.FiveHundred)]
         public void VersionedFactRule_CalculateWithoutVersionTestCase()
