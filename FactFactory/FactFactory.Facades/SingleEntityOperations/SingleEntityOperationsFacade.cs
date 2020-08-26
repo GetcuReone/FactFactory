dc2073ebcfc36cdc93d69ddec4d3530adc6eaf41
Itua Juel <getcureone@hotmail.com>
2020-08-26 16:40:37 +0300

    Move implimentation CompareFactRules

--- FactFactory/FactFactory.Facades/SingleEntityOperations/SingleEntityOperationsFacade.cs
+++ FactFactory/FactFactory.Facades/SingleEntityOperations/SingleEntityOperationsFacade.cs
@@ -29,60 +29,16 @@ namespace GetcuReone.FactFactory.Facades.SingleEntityOperations
         /// <summary>
         /// Compare <see cref="IFactRule"/>.
         /// </summary>
-        /// <param name="first"></param>
-        /// <param name="second"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public virtual int CompareFactRules<TFactRule, TWantAction, TFactContainer>(TFactRule first, TFactRule second, IWantActionContext<TWantAction, TFactContainer> context)
+        public virtual int CompareFactRules<TFactRule, TWantAction, TFactContainer>(TFactRule x, TFactRule y, IWantActionContext<TWantAction, TFactContainer> context)
             where TFactRule : IFactRule
             where TWantAction : IWantAction
             where TFactContainer : IFactContainer
         {
-            if ((first is IWantAction) || (second is IWantAction))
-                return 0;
-
-            if (first.InputFactTypes.IsNullOrEmpty())
-            {
-                if (second.InputFactTypes.IsNullOrEmpty())
-                    return 0;
-
-                return second.InputFactTypes.Any(factType => factType.IsFactType<ISpecialFact>())
-                    ? -1
-                    : 1;
-            }
-
-            if (second.InputFactTypes.IsNullOrEmpty())
-            {
-                return first.InputFactTypes.Any(factType => factType.IsFactType<ISpecialFact>())
-                    ? 1
-                    : -1;
-            }
-
-            int xCountCondition = first.InputFactTypes.Count(factType => factType.IsFactType<IConditionFact>());
-            int yCountCondition = second.InputFactTypes.Count(factType => factType.IsFactType<IConditionFact>());
-
-            if (xCountCondition != yCountCondition)
-            {
-                return xCountCondition > yCountCondition
-                    ? 1
-                    : -1;
-            }
-
-            int xCountSpecial = first.InputFactTypes.Count(factType => factType.IsFactType<ISpecialFact>());
-            int yCountSpecial = second.InputFactTypes.Count(factType => factType.IsFactType<ISpecialFact>());
-
-            if (xCountSpecial != yCountSpecial)
-            {
-                return xCountSpecial > yCountSpecial
-                    ? 1
-                    : -1;
-            }
-
-            if (first.InputFactTypes.Count > second.InputFactTypes.Count)
-                return -1;
-            if (first.InputFactTypes.Count < second.InputFactTypes.Count)
-                return 1;
-            return 0;
+            return x.CompareTo(y, context);
         }
 
         /// <inheritdoc/>
