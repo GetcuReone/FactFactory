9f3c93e8fd13a99289fc695e74a3234b9cd9971e
Itua Juel <getcureone@hotmail.com>
2020-08-27 19:41:15 +0300

    Added tests CompareFactsTests

--- FactFactory/PriorityFactFactory/FactFactory.PriorityTests/SingleEntityOperations/CompareFactsTests.cs
+++ FactFactory/PriorityFactFactory/FactFactory.PriorityTests/SingleEntityOperations/CompareFactsTests.cs
@@ -1,6 +1,7 @@
 ﻿using FactFactory.PriorityTests.CommonFacts;
 using FactFactory.PriorityTests.SingleEntityOperations.Env;
 using FactFactory.TestsCommon;
+using GetcuReone.FactFactory.Priority;
 using GetcuReone.GetcuTestAdapter;
 using GetcuReone.GwtTestFramework.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -19,6 +20,51 @@ namespace FactFactory.PriorityTests.SingleEntityOperations
             var fact1 = new FactResult(default);
             var fact2 = new FactResult(default);
             const int expectedValue = 0;
+
+            GivenCreateFacade()
+                .When("Compare fact.", facade => facade.CompareFacts(fact1, fact2))
+                .ThenAreEqual(expectedValue);
+        }
+
+        [TestMethod]
+        [TestCategory(TC.Objects.Fact), TestCategory(TC.Projects.Priority), TestCategory(GetcuReoneTC.Unit)]
+        [Description("Comparing facts with the same priority.")]
+        [Timeout(Timeouts.Millisecond.FiveHundred)]
+        public void WithSamePriorityTestCase()
+        {
+            var fact1 = new FactResult(default).SetPriority(new Priority1());
+            var fact2 = new FactResult(default).SetPriority(new Priority1());
+            const int expectedValue = 0;
+
+            GivenCreateFacade()
+                .When("Compare fact.", facade => facade.CompareFacts(fact1, fact2))
+                .ThenAreEqual(expectedValue);
+        }
+
+        [TestMethod]
+        [TestCategory(TC.Objects.Fact), TestCategory(TC.Projects.Priority), TestCategory(GetcuReoneTC.Unit)]
+        [Description("Comparison of facts with the first and second priorities.")]
+        [Timeout(Timeouts.Millisecond.FiveHundred)]
+        public void WithFirstAndSecondPrioritiesTestCase()
+        {
+            var fact1 = new FactResult(default).SetPriority(new Priority1());
+            var fact2 = new FactResult(default).SetPriority(new Priority2());
+            const int expectedValue = 1;
+
+            GivenCreateFacade()
+                .When("Compare fact.", facade => facade.CompareFacts(fact1, fact2))
+                .ThenAreEqual(expectedValue);
+        }
+
+        [TestMethod]
+        [TestCategory(TC.Objects.Fact), TestCategory(TC.Projects.Priority), TestCategory(GetcuReoneTC.Unit)]
+        [Description("Comparison of facts with the second and first priorities.")]
+        [Timeout(Timeouts.Millisecond.FiveHundred)]
+        public void WithSecondAndFirstPrioritiesTestCase()
+        {
+            var fact1 = new FactResult(default).SetPriority(new Priority2());
+            var fact2 = new FactResult(default).SetPriority(new Priority1());
+            const int expectedValue = -1;
 
             GivenCreateFacade()
                 .When("Compare fact.", facade => facade.CompareFacts(fact1, fact2))
