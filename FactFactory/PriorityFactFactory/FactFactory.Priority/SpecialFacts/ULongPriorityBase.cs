bc14c92573d12d5231ef22c279c033395f2cd36b
Itua Juel <getcureone@hotmail.com>
2020-08-27 15:53:17 +0300

    Added base classe for priority fact

---
+++ FactFactory/PriorityFactFactory/FactFactory.Priority/SpecialFacts/ULongPriorityBase.cs
@@ -0,0 +1,42 @@
+﻿using FactFactory.Priority.Interfaces;
+
+namespace GetcuReone.FactFactory.Priority.SpecialFacts
+{
+    /// <summary>
+    /// Base class for priority fact with value of type <see cref="uint"/>.
+    /// </summary>
+    public class ULongPriorityBase : PriorityBase<ulong>
+    {
+        /// <inheritdoc/>
+        public ULongPriorityBase(ulong value) : base(value)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override int CompareTo(IPriorityFact other)
+        {
+            switch (other)
+            {
+                case PriorityBase<int> priority:
+                    return priority.PriorityValue.CompareTo(PriorityValue);
+                case PriorityBase<uint> priority:
+                    return priority.PriorityValue.CompareTo(PriorityValue);
+                case PriorityBase<long> priority:
+                    return priority.PriorityValue.CompareTo(PriorityValue);
+                case PriorityBase<ulong> priority:
+                    return priority.PriorityValue.CompareTo(PriorityValue);
+
+                case FactBase<int> priority:
+                    return priority.Value.CompareTo(PriorityValue);
+                case FactBase<uint> priority:
+                    return priority.Value.CompareTo(PriorityValue);
+                case FactBase<long> priority:
+                    return priority.Value.CompareTo(PriorityValue);
+                case FactBase<ulong> priority:
+                    return priority.Value.CompareTo(PriorityValue);
+
+                default: throw CreateIncompatibilityVersionException(other);
+            }
+        }
+    }
+}
