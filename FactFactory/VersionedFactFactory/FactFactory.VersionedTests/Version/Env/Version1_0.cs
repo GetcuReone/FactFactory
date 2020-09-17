ec78b18ef8c9043db38d8cf1873397b989ebc3e1
Juel <GetcuReone@hotmail.com>
2020-09-17 18:42:01 +0300

    [versioned] Add base class for major-minor-patch version Fixes

---
+++ FactFactory/VersionedFactFactory/FactFactory.VersionedTests/Version/Env/Version1_0.cs
@@ -0,0 +1,11 @@
+﻿using GetcuReone.FactFactory.Versioned.SpecialFacts;
+
+namespace FactFactory.VersionedTests.Version.Env
+{
+    public class Version1_0 : MajorMinorPatchVersionBase
+    {
+        public Version1_0() : base("1.0")
+        {
+        }
+    }
+}
