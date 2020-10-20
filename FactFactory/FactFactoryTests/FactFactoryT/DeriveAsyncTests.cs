c18f00e43eea346e408d963a585c738f63113c83
Juel <GetcuReone@hotmail.com>
2020-10-21 02:08:17 +0300

    added RunningAsynchronousRulesInParallelTestCase

--- FactFactory/FactFactoryTests/FactFactoryT/DeriveAsyncTests.cs
+++ FactFactory/FactFactoryTests/FactFactoryT/DeriveAsyncTests.cs
@@ -1,6 +1,7 @@
 ﻿using FactFactory.TestsCommon;
 using FactFactory.TestsCommon.Helpers;
 using FactFactoryTests.CommonFacts;
+using GetcuReone.FactFactory.Interfaces;
 using GetcuReone.GetcuTestAdapter;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
@@ -111,6 +112,45 @@ namespace FactFactoryTests.FactFactoryT
                     {
                         await Task.Delay(Timeouts.Millisecond.Hundred);
                         return new Input16Fact(expectedValue);
+                    }
+                })
+                .And("Want actions.", factory => factory.WantFacts((Input16Fact fact) =>
+                {
+                    fact16 = fact;
+                }))
+                .WhenAsync("Derive.", factory => factory.DeriveAsync())
+                .Then("Check result.", () =>
+                {
+                    Assert.AreEqual(fact16, expectedValue);
+                })
+                .RunAsync();
+        }
+
+        [TestMethod]
+        [TestCategory(TC.Objects.Factory), TestCategory(GetcuReoneTC.Unit)]
+        [Description("Running asynchronous rules in parallel.")]
+        [Timeout(Timeouts.Millisecond.FiveHundred)]
+        public async Task RunningAsynchronousRulesInParallelTestCase()
+        {
+            Input16Fact fact16 = null;
+            const int expectedValue = 16;
+
+            await GivenCreateFactFactory()
+                .AndAddRules(new Collection
+                {
+                    {async () =>
+                    {
+                        await Task.Delay(Timeouts.Millisecond.Hundred);
+                        return new Input10Fact(10);
+                    }, FactWorkOption.CanExcecuteParallel | FactWorkOption.CanExecuteAsync},
+                    {async () =>
+                    {
+                        await Task.Delay(Timeouts.Millisecond.Hundred);
+                        return new Input6Fact(6);
+                    }, FactWorkOption.CanExcecuteParallel | FactWorkOption.CanExecuteAsync},
+                    (Input6Fact fact6, Input10Fact fact10) =>
+                    {
+                        return new Input16Fact(fact6 + fact10);
                     }
                 })
                 .And("Want actions.", factory => factory.WantFacts((Input16Fact fact) =>
