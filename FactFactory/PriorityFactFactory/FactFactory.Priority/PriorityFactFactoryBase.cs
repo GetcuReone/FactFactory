6ea43529617040a4e1de47129976280d720835b6
Juel Itua <GetcuReone@hotmail.com>
2022-08-25 21:42:53 +0300

    [support] Use base keyword as prefix on base entities Rename PriorityFactFactoryBase -> BasePriorityFactFactory #335

--- FactFactory/PriorityFactFactory/FactFactory.Priority/PriorityFactFactoryBase.cs
+++ FactFactory/PriorityFactFactory/FactFactory.Priority/PriorityFactFactoryBase.cs
@@ -1,23 +1,15 @@
-﻿using GetcuReone.FactFactory.BaseEntities;
-using GetcuReone.FactFactory.Interfaces.Operations;
-using GetcuReone.FactFactory.Priority.Facades.SingleEntityOperations;
+﻿using System;
+using GetcuReone.FactFactory.BaseEntities;
 
 namespace GetcuReone.FactFactory.Priority
 {
     /// <inheritdoc/>
-    public abstract class PriorityFactFactoryBase<TFactRule, TFactRuleCollection, TWantAction, TFactContainer> : FactFactoryBase<TFactRule, TFactRuleCollection, TWantAction, TFactContainer>
+    [Obsolete("Use BasePriorityFactFactory (deprecated in 4.0.2)")]
+    public abstract class PriorityFactFactoryBase<TFactRule, TFactRuleCollection, TWantAction, TFactContainer> : BasePriorityFactFactory<TFactRule, TFactRuleCollection, TWantAction, TFactContainer>
         where TFactRule : FactRuleBase
         where TFactRuleCollection : FactRuleCollectionBase<TFactRule>
         where TWantAction : WantActionBase
         where TFactContainer : FactContainerBase
     {
-        /// <summary>
-        /// Returns the <see cref="PrioritySingleEntityOperationsFacade"/>.
-        /// </summary>
-        /// <returns>Instance <see cref="PrioritySingleEntityOperationsFacade"/>.</returns>
-        protected override ISingleEntityOperations GetSingleEntityOperations()
-        {
-            return GetFacade<PrioritySingleEntityOperationsFacade>();
-        }
     }
 }
